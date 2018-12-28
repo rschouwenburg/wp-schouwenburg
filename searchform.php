0a48bc9a46f85b07a61141527cbc43ede4a510bb
Robert Schouwenburg <robert@schouwenburg.com>
2018-12-28 15:56:19 +0000

    Give search field autofocus on load

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>" autocomplete="off">
   <label>
   	<span class="screen-reader-text">Search for:</span>
-  	<input type="search" class="search-field" placeholder="Search &hellip;" value="<?php the_search_query(); ?>" name="s" />
+  	<input autofocus type="search" class="search-field" placeholder="Search &hellip;" value="<?php the_search_query(); ?>" name="s" />
   </label>
   <input type="submit" class="search-submit" value="Search" />
 </form>
