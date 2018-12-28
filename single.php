771f93aaa43e03b60eac4a94cb684058f4887d16
Robert Schouwenburg <robert@schouwenburg.com>
2018-12-28 16:59:20 +0000

    Show previous and next post and not post title for post navigation

--- single.php
+++ single.php
@@ -19,8 +19,18 @@ get_header();
 
 			get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation();
+			//the_post_navigation();
 
+			?>
+			<nav class="navigation post-navigation" role="navigation">
+				<h2 class="screen-reader-text">Post navigation</h2>
+				<div class="nav-links">
+					<div class="nav-previous"><?php previous_post_link('%link', '< PREVIOUS POST'); ?></div>
+					<div class="nav-next"><?php next_post_link('%link', 'NEXT POST >'); ?></div>
+				</div>
+			</nav>
+
+			<?php
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
