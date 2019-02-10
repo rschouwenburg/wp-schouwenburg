39b884b8b659bb58824a562863e41a2edae9d85a
Robert Schouwenburg <robert@schouwenburg.com>
2019-02-10 22:06:52 +0000

    Redo: Set content width to 700px. Now embedded content has the correct width.

--- functions.php
+++ functions.php
@@ -95,7 +95,7 @@ function schouwenburg_content_width() {
 	// This variable is intended to be overruled from themes.
 	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
 	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-	$GLOBALS['content_width'] = apply_filters( 'schouwenburg_content_width', 640 );
+	$GLOBALS['content_width'] = apply_filters( 'schouwenburg_content_width', 700 );
 }
 add_action( 'after_setup_theme', 'schouwenburg_content_width', 0 );
 
